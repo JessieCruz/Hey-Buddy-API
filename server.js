2460de0554ecb2d3685f337dac7a540cd182eaf3
Jessica Samara Cruz Pineda <jessiecruz@Jessicas-MacBook-Air.local>
2019-04-10 20:36:53 -0500

    commit port

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const {Persona} = require('./models/Personitas');
-const PORT = 3000; 
+// const cors = require('cors');
+const PORT = process.env.PORT || 3000; 
 
 const app = express();
 
